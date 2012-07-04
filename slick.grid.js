f6e15ad7cdb9027118634903b6067f4b7c8871f2
kianryan <kian@orangetentacle.co.uk>
2012-07-04 11:22:18 +0100

    Test commit

--- slick.grid.js
+++ slick.grid.js
@@ -16,6 +16,8 @@
  *     and do proper cleanup.
  */
 
+// Silly change to make a demo
+//
 // make sure required JavaScript modules are loaded
 if (typeof jQuery === "undefined") {
   throw "SlickGrid requires jquery module to be loaded";
